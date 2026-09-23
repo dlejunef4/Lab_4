5eec933d1c73a8453537f94cc3f19c573b69eaa9
dlejunef4 <davidlejunef1@gmail.com>
2026-09-23 15:49:11 -0500

    cashier notes and discount

--- Lab_4.cpp
+++ Lab_4.cpp
@@ -10,12 +10,14 @@ int main()
     char itemCode;
     int quantity;
     double unitPrice;
-    bool member;
+    char memberInput;
+    bool isMember = false;
+    string cashierNotes;
 
     cout << "Enter a food: ";                       // Gets Food input
     getline(cin,foodName);
 
-    cout << "Enter an item code(single digit): ";  // Gets Item code input
+    cout << "Enter an item code(single character): ";  // Gets Item code input
     cin >> itemCode;
 
     cout << "Enter the amount of items: ";        // Gets Amount of items
@@ -25,12 +27,30 @@ int main()
     cin >> unitPrice;
 
     cout << "Are you a member(y/n)? ";          // Asks if you're a member
-    cin >> member;
+    cin >> memberInput;
+
+
+    cout << "Enter cashier notes: ";
+    cin.ignore();
+    getline(cin, cashierNotes);
+
+
+    if (memberInput == 'y' || memberInput == 'Y') { // Checks if user is a member
+        isMember = true; 
+    }
 
     double subTotal = quantity * unitPrice;  //subtotal
+    double discount = isMember ? (subTotal * 0.10) : 0.0;
+    double total = subTotal - discount;
 
-    cout << fixed << setprecision(2);
-    cout << setw(28) << "Walmart Receipt" << endl;              // Receipt
+
+    cout << "\n\n";
+    cout << "========================================" << endl;
+    cout << "            PURCHASE RECEIPT            " << endl;
+    cout << "========================================" << endl;
+
+
+    cout << fixed << setprecision(2);             // Receipt
 
     cout << left <<setw(20) << "Item Name"
          << left <<setw(8) << "Code"
@@ -41,11 +61,21 @@ int main()
          << left <<setw(8) << itemCode
          << right << setw(6) << quantity
          << right << setw(10) << unitPrice << "\n" << endl;
+   cout << "========================================" << endl;
 
     cout << left << setw(25) << "Subtotal:"
          << right << setw(15) << subTotal << endl;
 
+    if (isMember) {
+     cout << left << setw(25) << "Discount Amount:"
+          << right << setw(11) << "-$" << discount << endl;
+    }
+     
+    cout << left << setw(25) << "Total Due:"
+          << right << setw(11) << "$" << total << endl;
 
+    cout << left << setw(25) << "Cashier Notes:"
+          << right << setw(11) << cashierNotes << endl;
 
 
 
